3dd9ccffc43297a56a488c11db1b552040f2b945
Yono Mittlefehldt <yono@toojuice.com>
2021-07-21 21:33:13 +0200

    Initial Commit

---
+++ Crushing Your Head/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Crushing Your Head
+//
+//  Created by Yonatan Mittlefehldt on 2021-21-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Crushing Your Head/Crushing_Your_HeadApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crushing_Your_HeadApp.swift
+//  Crushing Your Head
+//
+//  Created by Yonatan Mittlefehldt on 2021-21-07.
+//
+
+import SwiftUI
+
+@main
+struct Crushing_Your_HeadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
