064023a762ab23029425a38d4e812e25cbbd7058
Yono Mittlefehldt <yono@toojuice.com>
2021-07-21 21:50:11 +0200

    Add the initial implementation for the frame manager

---
+++ Crushing Your Head/Camera/FrameManager.swift
@@ -0,0 +1,52 @@
+//
+//  FrameManager.swift
+//  FrameManager
+//
+//  Created by Yonatan Mittlefehldt on 2021-21-07.
+//
+
+import AVFoundation
+
+class FrameManager: NSObject, ObservableObject {
+    static let shared = FrameManager()
+
+    @Published var error: FrameError?
+    @Published var current: CVPixelBuffer?
+
+    let videoOutputQueue = DispatchQueue(label: "app.CrushingYourHead.VideoOutputQ",
+                                         qos: .userInitiated,
+                                         attributes: [],
+                                         autoreleaseFrequency: .workItem)
+
+    private override init() {
+        super.init()
+
+        CameraManager.shared.set(self, queue: videoOutputQueue)
+    }
+
+    private func set(error: FrameError?) {
+        DispatchQueue.main.async {
+            self.error = error
+        }
+    }
+}
+
+extension FrameManager: AVCaptureVideoDataOutputSampleBufferDelegate {
+    func captureOutput(_ output: AVCaptureOutput,
+                       didOutput sampleBuffer: CMSampleBuffer,
+                       from connection: AVCaptureConnection) {
+        if sampleBuffer.numSamples == 0 {
+            set(error: .tooFewSamples)
+            return
+        } else if sampleBuffer.numSamples > 1 {
+            set(error: .tooManySamples)
+            return
+        }
+
+        if let buffer = sampleBuffer.imageBuffer {
+            DispatchQueue.main.async {
+                self.current = buffer
+            }
+        }
+    }
+}
