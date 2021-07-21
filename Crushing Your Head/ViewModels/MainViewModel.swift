1a6f16c8cba2062f9047213ed7b89a9f8dbe47a9
Yono Mittlefehldt <yono@toojuice.com>
2021-07-21 22:22:42 +0200

    Add the view model that will control the main view for the app

---
+++ Crushing Your Head/ViewModels/MainViewModel.swift
@@ -0,0 +1,41 @@
+//
+//  MainViewModel.swift
+//  MainViewModel
+//
+//  Created by Yonatan Mittlefehldt on 2021-21-07.
+//
+
+import Combine
+import CoreImage
+
+class MainViewModel: ObservableObject {
+    @Published var error: Error?
+    @Published var frame: CGImage?
+
+    private let cameraManager = CameraManager.shared
+    private let frameManager = FrameManager.shared
+
+    private var subscriptions = Set<AnyCancellable>()
+
+    init() {
+        setupSubscriptions()
+    }
+
+    private func setupSubscriptions() {
+        cameraManager.$error
+            .receive(on: RunLoop.main)
+            .sink { self.error = $0 }
+            .store(in: &subscriptions)
+
+        frameManager.$error
+            .receive(on: RunLoop.main)
+            .sink { self.error = $0 }
+            .store(in: &subscriptions)
+
+        // Reset the error after 10 seconds of no new error
+        $error
+            .debounce(for: .seconds(10), scheduler: DispatchQueue.main)
+            .sink { _ in self.error = nil }
+            .store(in: &subscriptions)
+    }
+}
