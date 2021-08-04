280635c0c6b08ca038f68341921b4addefda361a
Yono Mittlefehldt <yono@toojuice.com>
2021-08-04 21:33:11 +0200

    Add a heads up display to show the score

---
+++ Crushing Your Head/Views/Components/HUDView.swift
@@ -0,0 +1,39 @@
+//
+//  HUDView.swift
+//  HUDView
+//
+//  Created by Yonatan Mittlefehldt on 2021-04-08.
+//
+
+import SwiftUI
+
+struct HUDView: View {
+    var score: Int
+    var body: some View {
+        VStack {
+            HStack {
+                Spacer()
+                
+                HStack {
+                    Text("SCORE:")
+                        .fontWeight(.bold)
+                    
+                    Text("\(score)")
+                        .fontWeight(.bold)
+                        .frame(width: 75, alignment: .trailing)
+                }
+                .foregroundColor(.white)
+                .shadow(color: .black.opacity(0.7), radius: 2, x: 0, y: 0)
+                .padding(.horizontal)
+            }
+            
+            Spacer()
+        }
+    }
+}
+
+struct HUDView_Previews: PreviewProvider {
+    static var previews: some View {
+        HUDView(score: 999999)
+    }
+}
