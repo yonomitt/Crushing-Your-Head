caca9494980d8863fc2a99bb56e15fb9f6159809
Yono Mittlefehldt <yono@toojuice.com>
2021-08-04 21:35:48 +0200

    Extend `Publisher` with the nwise function from the wonderful CombineExt repo

--- Crushing Your Head/Extensions/PublisherExtension.swift
+++ Crushing Your Head/Extensions/PublisherExtension.swift
@@ -8,6 +8,24 @@
 import Combine
 
 extension Publisher {
+    /// From: https://github.com/CombineCommunity/CombineExt
+    /// Groups the elements of the source publisher into arrays of N consecutive elements.
+    /// The resulting publisher:
+    ///    - does not emit anything until the source publisher emits at least N elements;
+    ///    - emits an array for every element after that;
+    ///    - forwards any errors or completed events.
+    ///
+    /// - parameter size: size of the groups, must be greater than 1
+    ///
+    /// - returns: A type erased publisher that holds an array with the given size.
+    func nwise(_ size: Int) -> AnyPublisher<[Output], Failure> {
+        assert(size > 1, "n must be greater than 1")
+
+        return scan([]) { acc, item in Array((acc + [item]).suffix(size)) }
+            .filter { $0.count == size }
+            .eraseToAnyPublisher()
+    }
+
     /// Creates a Sample publisher, which emits the upstream publisher's every Xth value
     /// - Parameters:
     ///   - count: how often to sample the publisher
