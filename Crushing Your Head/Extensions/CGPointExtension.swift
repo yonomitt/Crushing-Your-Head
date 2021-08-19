a2f3151f123f21eaeb9d3b47dae48feb1f71e756
Yono Mittlefehldt <yono@toojuice.com>
2021-08-19 21:41:37 +0200

    Add new operators to CGPoint to handle multiplying and dividing by a scaler

--- Crushing Your Head/Extensions/CGPointExtension.swift
+++ Crushing Your Head/Extensions/CGPointExtension.swift
@@ -12,6 +12,14 @@ extension CGPoint {
         CGPoint(x: point.x * size.width, y: point.y * size.height)
     }
 
+    static func * (point: CGPoint, multiplier: CGFloat) -> CGPoint {
+        CGPoint(x: point.x * multiplier, y: point.y * multiplier)
+    }
+
+    static func / (point: CGPoint, divisor: CGFloat) -> CGPoint {
+        point * (1.0 / divisor)
+    }
+
     static func + (lhs: CGPoint, rhs: CGPoint) -> CGPoint {
         CGPoint(x: lhs.x + rhs.x, y: lhs.y + rhs.y)
     }
